58c003dc2d96e4eb314edc7a13d6b03b5a0f4475
Amy Cen <amy.cen@blackrock.com>
2019-07-07 18:15:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function createEmployeeRecord(arr) {
   return {
     firstName: arr[0],
     familyName: arr[1],
-    payPerHour: arr[3]
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
   }
 }
